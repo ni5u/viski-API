a19b5e2e76fdeb5ccdec0158a43b3ca2d2a5dbc3
Henri Mäkelä <henri.makela@boogiesoftware.com>
2019-06-24 15:12:02 +0300

    removed mongodb creds

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb+srv://henriuser:henriuserwhiskeyboy@cluster0-l0rpu.mongodb.net/test?retryWrites=true');
+mongoose.connect('db url');
 
 
 module.exports = {
